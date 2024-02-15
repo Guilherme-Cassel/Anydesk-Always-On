2e6fc7edae2045f2e8561a17301a6ce1385965fb
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2024-02-15 17:18:47 -0300

    Finished Backend!!

---
+++ AnyDesk Always On/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AnydeskAlwaysOn;
+
+class Program : ProcessManager
+{
+    static void Main()
+    {
+        if (ListProcess(AppDomain.CurrentDomain.FriendlyName).Count > 1)
+            return;
+
+        ProcessAlwaysOn processAlwaysOn = new
+        (
+            "AnyDesk.exe"
+        );
+        processAlwaysOn.Start();
+    }
+}
--- AnydeskAlwaysOn/Program.cs
+++ AnydeskAlwaysOn/Program.cs
@@ -1,88 +1,16 @@
-﻿using AnydeskAlwaysOn.Properties;
-using System.Diagnostics;
+﻿namespace AnydeskAlwaysOn;
 
-namespace AnydeskAlwaysOn;
-class Program
+class Program : ProcessManager
 {
-    public static event EventHandler? ProcessDied;
-
     static void Main()
     {
-        AnyDeskProcessManager.EnsureSingleInstance();
-
-        ProcessDied += Process_Died;
-
-        CheckIfAnyDeskExecutableExists();
-
-        MakeSureThereIsAnInstanceOfAnyDeskOpen();
-
-        AttachEventToProcess(MainProcess("AnyDesk"), ProcessDied);
-    }
-
-
-
-    ////////////////////////////////////////////////////////////////
-
-    private static void MakeSureThereIsAnInstanceOfAnyDeskOpen()
-    {
-        
-    }
-
-    private static void CheckIfAnyDeskExecutableExists()
-    {
-        if (!File.Exists(AnyDeskExecutablePath))
-        {
-            CopyNeededFiles(AnyDeskExecutablePath);
-        }
-    }
-
-    private static void CopyNeededFiles(string destiny)
-    {
-        var DirectoryPathWithoutSufix = destiny.Remove(destiny.IndexOf("AnyDesk.exe"));
-
-        if (NeededFiles == null)
+        if (ListProcess(AppDomain.CurrentDomain.FriendlyName).Count > 1)
             return;
 
-        Directory.CreateDirectory(DirectoryPathWithoutSufix);
-
-        DirectoryInfo directory = new DirectoryInfo(NeededFiles);
-        foreach (FileInfo file in directory.GetFiles())
-        {
-            file.CopyTo(DirectoryPathWithoutSufix + file.Name, false);
-        }
-    }
-
-    private static void AttachEventToProcess(Process process, EventHandler? e)
-    {
-        Process isSelectedProcess = Process.GetProcessById(process.Id);
-
-        if (isSelectedProcess != null)
-        {
-            isSelectedProcess.WaitForExit();
-        }
-        if (e != null)
-        {
-            e.Invoke(isSelectedProcess, new EventArgs());
-        }
-    }
-
-    private static void Process_Died(object? sender, EventArgs e)
-    {
-#if DEBUG
-        Console.WriteLine("Anydesk Closed!! Re-opening...");
-#endif
-        List<Process> leftOverAnyDeskProcesses = ListProcess("AnyDesk");
-        foreach (Process anydeskProcess in leftOverAnyDeskProcesses)
-        {
-            anydeskProcess.Kill(); // kill the leftover sub-processes of anydesk
-        }
-
-        Thread.Sleep(1000);
-
-        Process process = Process.Start(AnyDeskExecutablePath);
-
-        Thread.Sleep(1000);
-
-        AttachEventToProcess(process /*firt index is the key process!*/, ProcessDied);
+        ProcessAlwaysOn processAlwaysOn = new
+        (
+            "AnyDesk.exe"
+        );
+        processAlwaysOn.Start();
     }
 }
