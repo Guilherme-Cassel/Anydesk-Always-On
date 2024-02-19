ba159cce12b9706c6060b1b6f541f15539c5a5cf
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2024-02-19 09:12:04 -0300

    Added Shortcut creator

---
+++ AnyDesk Always On/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using IWshRuntimeLibrary;
+
+namespace AnyDesk_Always_On;
+
+public static class FileManager
+{
+    private static readonly string StartupShortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), "AnyDesk Always On.lnk");
+    private static readonly string CurrentExePath = Environment.ProcessPath!;
+    public static void CreateShortcutOnStartup()
+    {
+        if (System.IO.File.Exists(StartupShortcutPath))
+            return;
+
+        IWshShell_Class wshShell = new();
+
+        IWshShortcut shortcut = (IWshShortcut)wshShell.CreateShortcut(StartupShortcutPath);
+
+        shortcut.TargetPath = CurrentExePath;
+
+        shortcut.Save();
+    }
+}
