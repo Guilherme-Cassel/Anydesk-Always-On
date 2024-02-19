f0d053ed0baf7f07352fab333193ecb7a4ee6970
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2024-02-19 09:10:55 -0300

    Added FatalException

--- AnyDesk Always On/Exceptions.cs
+++ AnyDesk Always On/Exceptions.cs
@@ -1,6 +1,8 @@
 ﻿namespace AnyDesk_Always_On;
 
-public class ProcessCapturingException(Exception exception) : Exception(@"Houve um erro ao capturar os processos referentes ao AnyDesk Always On", exception)
-{ }
+public class ProcessCapturingException(Exception exception) : Exception(@"Houve um erro ao capturar os processos referentes ao AnyDesk Always On", exception);
 
 public class HandledException(Exception exception) : Exception("Erro Capturado com Sucesso! Contate seu TI\nErro:", exception);
+
+public class HandledFatalException(Exception exception) : Exception("Erro Fatal Capturado com Sucesso!\nA Aplicação será Fechada, Para Ajuda, Contate seu TI\nErro:", exception);
+
